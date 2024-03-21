2d27f2cb6626e0af1fe60bc179ce6589633d2d5b
Nimi Williams <williamson.nimi@gmail.com>
2024-03-21 19:44:31 +0100

    Created a new folder and 2 exercises to create tables

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,2 @@
+-- This is a script that creates a table regardless of the DB and adds enums
+CREATE TABLE (IF NOT EXISTS) users (id INT NOT NULL AUTO_INCREMENT, email VARCHAR(255)NOT NULL UNIQUE, name VARCHAR(255), country ENUM('US', 'CO', 'TN') DEFAULT 'US', PRIMARY KEY(id)) 
