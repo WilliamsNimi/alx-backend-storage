b3889ceaa3fecd2cbdd62f3b1506ad95abf13346
Nimi Williams <williamson.nimi@gmail.com>
2024-03-21 19:49:40 +0100

    Trying a fix with the removal of parenthesis

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,2 +1,6 @@
 -- This is a script that creates a table regardless of the DB
-CREATE TABLE (IF NOT EXISTS) users (id INT NOT NULL AUTO_INCREMENT, email VARCHAR(255)NOT NULL UNIQUE, name VARCHAR(255), PRIMARY KEY(id)) 
+CREATE TABLE IF NOT EXISTS users (
+id INT NOT NULL AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255), PRIMARY KEY(id)
+); 
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,2 +1,8 @@
 -- This is a script that creates a table regardless of the DB and adds enums
-CREATE TABLE (IF NOT EXISTS) users (id INT NOT NULL AUTO_INCREMENT, email VARCHAR(255)NOT NULL UNIQUE, name VARCHAR(255), country ENUM('US', 'CO', 'TN') DEFAULT 'US', PRIMARY KEY(id)) 
+CREATE TABLE IF NOT EXISTS users (
+id INT NOT NULL AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM('US', 'CO', 'TN') DEFAULT 'US',
+PRIMARY KEY(id)
+); 
